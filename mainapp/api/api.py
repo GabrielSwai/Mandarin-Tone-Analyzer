e813837e1e62885fccc59e04dfca7f57f169f6da
Gabriel Swai <gabrielswai@game.local>
2026-01-22 16:33:03 -0500

    Added small change to connect a phrase ID to file names.

--- mainapp/api/api.py
+++ mainapp/api/api.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, send_from_directory, request, jsonify, url_for
+from flask import Blueprint, send_from_directory, request, jsonify, url_for, json
 from pathlib import Path
 from werkzeug.utils import secure_filename
 from uuid import uuid4
@@ -27,6 +27,9 @@ def upload():
     base = secure_filename(Path(f.filename).stem)[:40] or "rec" # create base name for disk readability; `secure_filename` strips weird chars
 
     out_name = f"{uuid4().hex}__{base}{ext}" # create unique filenames
+
+    phrase_id = request.form.get("phrase_id", "") # initialize `phrase_id` var
+    (UPLOAD_DIR / f"{out_name}.json").write_text(json.dumps("phrase_id", phrase_id)) # write the phrase_id to the proper file
 
     out_path = UPLOAD_DIR / out_name # save file to disk
     f.save(out_path)
